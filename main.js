48aa7850aa8d03b5a5dff3e1711df13ca7598183
Alberto Zúñiga <azuiga@miuandes.cl>
2024-06-09 11:25:05 -0400

    updates

--- main.js
+++
@@ -1,7 +0,0 @@
-var boton = document.getElementById("cambiarTexto");
-var parrafo = document.getElementById("textoVariable");
-
-boton.addEventListener("click", function() {
-  var nuevoParrafo = document.getElementById("nuevoTexto").value;
-  document.getElementById("textoVariable").innerHTML = nuevoParrafo;
-},true);
