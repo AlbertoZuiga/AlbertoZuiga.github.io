f913b53f429b0828a62bfce5c4bad58911ef00b0
Alberto Zúñiga <azuiga@miuandes.cl>
2024-06-12 10:43:18 -0400

    test

---
+++ loadData.js
@@ -0,0 +1,26 @@
+const jsDiv = document.getElementById("js");
+const jsFiles = ["loadStyles.js", "toggle-button.js"];
+jsFiles.forEach((file) => {
+  const src = "./js/" + file;
+  const script = document.createElement("script");
+  script.src = src;
+
+  jsDiv.appendChild(script);
+});
+
+const cssFiles = [
+  "class-styles.css",
+  "footer-styles.css",
+  "nav-styles.css",
+  "table-styles.css",
+  "tag-styles.css",
+  "toggle-styles.css",
+];
+cssFiles.forEach((file) => {
+  const href = "./styles/" + file;
+  const link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href = href;
+
+  document.head.appendChild(link);
+});
