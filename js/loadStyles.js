520a5b297cc2cab060599ca6a51ca815f6cd260e
Alberto Zúñiga <azuiga@miuandes.cl>
2024-06-12 10:33:56 -0400

    corrected

--- js/loadStyles.js
+++ js/loadStyles.js
@@ -1,25 +1,23 @@
 // Función para cargar archivos CSS dinámicamente
-function loadCSSFilesFromDirectory(directory) {
-    // Obtener todos los archivos en el directorio
-    fetch(directory)
-        .then(response => response.text())
-        .then(text => {
-            // Utilizar una expresión regular para encontrar los archivos CSS
-            const cssFiles = text.match(/href="([^"]+\.css)"/g);
-            // Cargar cada archivo CSS
-            if (cssFiles) {
-                cssFiles.forEach(file => {
-                    const href = file.match(/href="([^"]+\.css)"/)[1];
-                    const link = document.createElement('link');
-                    link.rel = 'stylesheet';
-                    link.href = href;
 
-                    document.head.appendChild(link);
-                });
-            }
-        })
-        .catch(error => console.error('Error al cargar archivos CSS:', error));
+function loadCSSFiles() {
+  const cssFiles = [
+    "class-styles.css",
+    "footer-styles.css",
+    "nav-styles.css",
+    "table-styles.css",
+    "tag-styles.css",
+    "toggle-styles.css",
+  ];
+  cssFiles.forEach((file) => {
+    const href = "./styles/" + file
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = href;
+
+    document.head.appendChild(link);
+  });
 }
 
 // Llamar a la función para cargar los archivos CSS
-loadCSSFilesFromDirectory('styles/');
+loadCSSFiles();
--- loadScripts.js
+++ loadScripts.js
@@ -1,28 +1,16 @@
-// Función para cargar archivos js dinámicamente
-function loadJsFilesFromDirectory(directory) {
-    // Obtener todos los archivos en el directorio
-    fetch(directory)
-        .then(response => response.text())
-        .then(text => {
-            // Utilizar una expresión regular para encontrar los archivos js
-            const jsFiles = text.match(/href="([^"]+\.js)"/g);
-            // Cargar cada archivo js
+function loadJsFiles() {
+  const jsDiv = document.getElementById("js");
+  const jsFiles = [
+        "loadStyles.js",
+        "toggle-button.js"
+    ];  
+  jsFiles.forEach((file) => {
+    const src = "js/" + file;
+    const script = document.createElement("script");
+    script.src = src;
 
-            const jsDiv = document.getElementById("js")
-            if (jsFiles) {
-                jsFiles.forEach(file => {
-                    const src = file.match(/href="([^"]+\.js)"/)[1];
-                    const script = document.createElement('script');
-                    // script.rel = 'stylesheet';
-                    script.src = src;
-
-                    jsDiv.appendChild(script);
-                });
-            }
-        })
-        .catch(error => console.error('Error al cargar archivos js:', error));
+    jsDiv.appendChild(script);
+  });
 }
 
-// Llamar a la función para cargar los archivos js
-console.log("Esto es una prueba");
-loadJsFilesFromDirectory('./js/');
+loadJsFiles();
