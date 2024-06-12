d9e29e1ce55cf80362db66680b4ec734ff43035e
Alberto Zúñiga <azuiga@miuandes.cl>
2024-06-12 10:36:59 -0400

    test

--- loadScripts.js
+++ loadScripts.js
@@ -1,11 +1,8 @@
 function loadJsFiles() {
   const jsDiv = document.getElementById("js");
-  const jsFiles = [
-        "loadStyles.js",
-        "toggle-button.js"
-    ];  
+  const jsFiles = ["loadStyles.js", "toggle-button.js"];
   jsFiles.forEach((file) => {
-    const src = "js/" + file;
+    const src = "./js/" + file;
     const script = document.createElement("script");
     script.src = src;
 
