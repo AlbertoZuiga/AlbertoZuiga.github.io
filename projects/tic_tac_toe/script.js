79aea3341836a1632d48ff5cc96cc1c9bd02c347
Alberto Zúñiga <azuiga@miuandes.cl>
2024-06-22 19:12:02 -0400

    improoved tic tac toe code

--- projects/tic_tac_toe/script.js
+++ projects/tic_tac_toe/script.js
@@ -1,171 +1,106 @@
-let gameBoard = document.getElementById("game");
-let SCALER = 0.8;
-let SIZE = { x: 0, y: 0, width: 0, height: 0 };
-const body = document.querySelector("body");
+const CELLS = document.querySelectorAll("[data-cell]");
+const BOARD = document.getElementById("board");
+const RESTART_BUTTON = document.getElementById("restartButton");
+let isOTurn = false;
+let gameOn = true;
+const X_CLASS = "X";
+const O_CLASS = "O";
+const WINNING_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2],
+];
 
-let playing = true;
-let playerXTurn = true;
-const restart = document.getElementById("restart");
+startGame();
 
-restart.addEventListener("click", () => {
-  document.querySelectorAll(".X").forEach((element) => {
-    element.classList.remove("X");
-    element.classList.add("clear");
-    element.textContent = "";
-  });
+RESTART_BUTTON.addEventListener("click", startGame);
 
-  document.querySelectorAll(".O").forEach((element) => {
-    element.classList.remove("O");
-    element.classList.add("clear");
-    element.textContent = "";
-  });
-
-  restart.classList.add("hidden");
-  playing = true;
-});
-
-function displayButton() {
-  restart.classList.remove("hidden");
-}
-
-document.querySelectorAll(".box").forEach((element) => {
-  element.addEventListener("click", function (event) {
-    // Aquí puedes acceder al div
-    const div = event.target;
-
-    if (div.classList.contains("clear") && playing) {
-      div.classList.remove("clear");
-      playerXTurn ? div.classList.add("X") : div.classList.add("O");
-      let player = playerXTurn ? "X" : "O";
-      div.textContent = player;
-      playerXTurn = !playerXTurn;
-
-      if (checkWin(player)) {
-        setTimeout(alert, 10, `Gano ${player}`);
-      }
-      if (document.querySelector(".clear") == null && playing) {
-        setTimeout(alert, 10, "Empate");
-        playing = false;
-      }
-
-      if (!playing) displayButton();
-    }
-  });
-});
-
-function checkWin(player) {
-  let playerWin = true;
-
-  document.querySelectorAll(".top").forEach((element) => {
-    if (!element.classList.contains(player)) {
-      playerWin = false;
-    }
-  });
-  if (playerWin) {
-    playing = false;
-    return playerWin;
-  } else {
-    playerWin = true;
-  }
-
-  document.querySelectorAll(".middle").forEach((element) => {
-    if (!element.classList.contains(player)) {
-      playerWin = false;
-    }
-  });
-  if (playerWin) {
-    playing = false;
-    return playerWin;
-  } else {
-    playerWin = true;
-  }
-
-  document.querySelectorAll(".down").forEach((element) => {
-    if (!element.classList.contains(player)) {
-      playerWin = false;
-    }
-  });
-  if (playerWin) {
-    playing = false;
-    return playerWin;
-  } else {
-    playerWin = true;
-  }
-
-  document.querySelectorAll(".left").forEach((element) => {
-    if (!element.classList.contains(player)) {
-      playerWin = false;
-    }
-  });
-  if (playerWin) {
-    playing = false;
-    return playerWin;
-  } else {
-    playerWin = true;
-  }
-
-  document.querySelectorAll(".center").forEach((element) => {
-    if (!element.classList.contains(player)) {
-      playerWin = false;
-    }
-  });
-  if (playerWin) {
-    playing = false;
-    return playerWin;
-  } else {
-    playerWin = true;
-  }
-
-  document.querySelectorAll(".right").forEach((element) => {
-    if (!element.classList.contains(player)) {
-      playerWin = false;
-    }
-  });
-  if (playerWin) {
-    playing = false;
-    return playerWin;
-  } else {
-    playerWin = true;
-  }
-
-  if (document.querySelector(".middle.center").classList.contains(player)) {
-    if (
-      (document.querySelector(".right.top").classList.contains(player) &&
-        document.querySelector(".left.down").classList.contains(player)) ||
-      (document.querySelector(".left.top").classList.contains(player) &&
-        document.querySelector(".right.down").classList.contains(player))
-    ) {
-      playing = false;
-      return true;
-    }
-  }
-  return false;
-}
-
-window.addEventListener("resize", handleResize);
-
-function handleResize() {
-  let resizer = SCALER * Math.min(body.offsetWidth, body.offsetHeight);
-  SIZE.width = resizer;
-  SIZE.height = resizer;
-  SIZE.x = body.offsetWidth / 2 - SIZE.width / 2;
-  SIZE.y = body.offsetHeight / 2 - SIZE.height / 2;
-
-  gameBoard.style.left = `${SIZE.x}px`;
-  gameBoard.style.top = `${SIZE.y}px`;
-
-  gameBoard.style.width = `${SIZE.width}px`;
-  gameBoard.style.height = `${SIZE.height}px`;
-
-  const cellWidth =
-    (SIZE.width - 2 * parseFloat(getComputedStyle(gameBoard).gap)) / 3;
-  const cellHeight =
-    (SIZE.height - 2 * parseFloat(getComputedStyle(gameBoard).gap)) / 3;
-
-  Array.from(gameBoard.children).map(function (element) {
-    element.style.width = `${cellWidth}px`;
-    element.style.height = `${cellHeight}px`;
+function startGame() {
+  isOTurn = false;
+  CELLS.forEach((cell) => {
+    cell.classList.remove(X_CLASS);
+    cell.classList.remove(O_CLASS);
+    cell.classList.remove("win");
+    cell.textContent = "";
+    cell.removeEventListener("click", handleClick);
+    cell.addEventListener("click", handleClick, { once: true });
   });
 }
 
-handleResize();
+function handleClick(e) {
+  if (!gameOn) return;
+  const cell = e.target;
+  const currentClass = isOTurn ? O_CLASS : X_CLASS;
+  placeMark(cell, currentClass);
+  if (checkWin(currentClass)) {
+    setTimeout(endGame, 10, false);
+  } else if (isDraw()) {
+    setTimeout(endGame, 10, true);
+  } else {
+    swapTurns();
+  }
+}
+
+function placeMark(cell, currentClass) {
+  cell.classList.add(currentClass);
+  cell.textContent = currentClass.toUpperCase();
+}
+
+function swapTurns() {
+  isOTurn = !isOTurn;
+}
+
+function checkWin(currentClass) {
+  let winningCombination = null;
+  const isWin = WINNING_COMBINATIONS.some((combination) => {
+    const isWinningCombination = combination.every((index) => {
+      return CELLS[index].classList.contains(currentClass);
+    });
+    if (isWinningCombination) {
+      winningCombination = combination;
+    }
+    return isWinningCombination;
+  });
+
+  if (isWin) {
+    winEffect(winningCombination);
+  }
+
+  gameOn = !isWin;
+  return isWin;
+}
+
+function isDraw() {
+  return [...CELLS].every((cell) => {
+    return cell.classList.contains(X_CLASS) || cell.classList.contains(O_CLASS);
+  });
+}
+
+function endGame(draw) {
+  if (draw) {
+    console.log("Empate!");
+  } else {
+    console.log(`Gano ${isOTurn ? "O" : "X"}!`);
+  }
+}
+
+function winEffect(combination) {
+  let index;
+  for (let i in combination) {
+    setTimeout(() => {
+      index = combination[i];
+      CELLS[index].classList.add("win");
+      CELLS[index].style.transform = "scale(1.1)";
+    }, 400 * i);
+
+    setTimeout(() => {
+      index = combination[i];
+      CELLS[index].style.transform = "scale(1)";
+    }, 400 * i + 200);
+  }
+}
